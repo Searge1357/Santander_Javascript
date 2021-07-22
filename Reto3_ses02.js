f66adf53071fbee71e599eee22d40e2272efd65a
Sergio <a00825769@itesm.mx>
2021-07-21 22:13:22 -0500

    Retos 2 y 3 sesion 2 Otra vez

--- Reto3_ses02.js
+++ Reto3_ses02.js
@@ -15,3 +15,20 @@ while (i <= 100){
     }
     i++; 
 }
+
+
+/* SOlución del profe 
+
+for (let counter = 2; counter <= 100; counter++) {
+  let isPrime = true;
+
+  for (let i = 2; i <= counter; i++) {
+    if (counter % i === 0 && i !== counter) {
+      isPrime = false;
+    }
+  }
+
+  if(isPrime) console.log(counter)
+}
+
+*/
